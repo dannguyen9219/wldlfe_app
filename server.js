3007453d98fa9063dbc296de838d8c4dc3128433
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-03-04 14:20:57 -0600

    A start of something new - installed dependencies

---
+++ server.js
@@ -0,0 +1,45 @@
+// Boilerplate Dependencies //
+require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// Database Connection //
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+// Establish Connection //
+mongoose.connect(DATABASE_URL, CONFIG);
+
+// Events for when connection opens/disconnects/error //
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+;
+
+// Views Engine - App Object Engine //
+const app = express();
+app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'jsx');
+
+// Middleware //
+app.use(morgan('tiny'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+// Listening Route //
+app.get('/', (req, res) => {
+    res.send(`Wldlfe server is running`)
+});
+
+
+// Listening to PORT 8000 //
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
